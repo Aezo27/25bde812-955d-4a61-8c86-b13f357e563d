d255e0f636b3db17420e0c90967b13c464e3e143
Rama Sullivan <ramasullivan27@gmail.com>
2023-12-12 01:49:13 +0700

    update tooltip with interface

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -1,8 +1,12 @@
-const Tooltip = (props: any) => {
+interface TooltipProps{
+  content: string | number,
+}
+
+const Tooltip: React.FC<TooltipProps> = ({content}) => {
   return (
     <div className="absolute bottom-[calc(100%+0.5rem)] left-[50%] -translate-x-[50%] hidden group-hover:block w-auto">
       <div className="bottom-full right-0 rounded bg-black dark:bg-gray-600 px-4 py-1 text-xs text-white whitespace-nowrap">
-        {props.text}
+        {content}
         <svg
           className="absolute left-0 top-full h-2 w-full text-black dark:text-gray-600"
           x="0px"
